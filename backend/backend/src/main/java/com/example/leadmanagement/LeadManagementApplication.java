b2d6ccf4af3429952a7ad2471306bd1d5fde2292
yeturilatha <149352970+yeturilatha@users.noreply.github.com>
2025-12-18 23:06:53 +0530

    Create LeadManagementApplication.java

---
+++ backend/backend/src/main/java/com/example/leadmanagement/LeadManagementApplication.java
@@ -0,0 +1,12 @@
+package com.example.leadmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeadManagementApplication.class, args);
+    }
+}
